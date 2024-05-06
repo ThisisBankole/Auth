373580785f6c007ad365a3a749cf0b12f4247631
thisisbankole <bjamgbadi@gmail.com>
2024-05-06 14:32:35 +0100

    initial

---
+++ Auth/Pages/account/Login.cshtml.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace MyApp.Namespace
+{
+    public class LoginModel : PageModel
+
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }        
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+            
+                // Check if the user is authenticated
+            if (Credential.Username == "admin" && Credential.Password == "admin")
+            {
+                // Create Security Context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@mywebsite.com")
+                };
+                var claimsIdentity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+                
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        public required string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+    }
+}
